828a2b058ee287763b938b9b9d8905ba989947c2
Matt Hanvelt <mhanvelt@mac.com>
2020-10-22 13:00:14 -0500

    Updated nupkg to 1.0.4, changed DbProviderFactories to cache factory instances, allow singleton instances to be provided as-is

--- ADORE.Standard/DbProviderFactories.cs
+++ ADORE.Standard/DbProviderFactories.cs
@@ -7,17 +7,45 @@ namespace ADORE.Standard
 {
 	public static class DbProviderFactories
 	{
-		private static Dictionary<string,Type> _providers = new Dictionary<string, Type>();
+		private static Dictionary<string, DbProviderFactory> _providers = new Dictionary<string, DbProviderFactory>();
 
 		/// <summary>
-		/// <para>Registers a DbProviderFactory implementation with a given provider name.</para>
+		/// <para>Registers a DbProviderFactory implementation with a given provider name. Assumes that the factory is NOT implemented as a singleton instance.</para>
 		/// </summary>
 		/// <param name="providerName">The name to register this provider with</param>
 		/// <param name="dbProviderFactoryType">The DbProviderFactory class name being registered</param>
 		/// <exception cref="ArgumentNullException">Thrown if dbProviderFactoryType is null</exception>
 		public static void RegisterFactory(string providerName, Type dbProviderFactoryType)
 		{
-			_providers[providerName] = dbProviderFactoryType ?? throw new ArgumentNullException("dbProviderFactoryType");
+			if(_providers.ContainsKey(providerName)) { throw new InvalidOperationException(string.Format("Factory for provider {0} is already registered.", providerName)); }
+			if(string.IsNullOrEmpty(providerName)) { throw new InvalidOperationException(string.Format("Factory for provider {0} is not valid.", providerName)); }
+			if(!dbProviderFactoryType.IsSubclassOf(typeof(DbProviderFactories))) { throw new InvalidOperationException(string.Format("{0} is not a System.Data.Common.DbProviderFactory.", dbProviderFactoryType.FullName)); }
+
+			_providers[providerName] = (DbProviderFactory)Activator.CreateInstance(dbProviderFactoryType);
+		}
+		/// <summary>
+		/// <para>Registers a DbProviderFactory implementation with a given provider name. Assumes that the factory is NOT implemented as a singleton instance.</para>
+		/// </summary>
+		/// <param name="providerName">The name to register thisprovider with</param>
+		/// <param name="singletonInstance">The instance of a DbProviderFactory class being registered</param>
+		/// <exception cref="ArgumentNullException">Thrown if dbProviderFactoryType is null</exception>
+		public static void RegisterFactory(string providerName, DbProviderFactory singletonInstance)
+		{
+			if(_providers.ContainsKey(providerName)) { throw new InvalidOperationException(string.Format("Factory for provider {0} is already registered.", providerName)); }
+			if(string.IsNullOrEmpty(providerName)) { throw new InvalidOperationException(string.Format("Factory for provider {0} is not valid.", providerName)); }
+			if(singletonInstance == null) { throw new InvalidOperationException(string.Format("A null instance was provided and cannot be registered.")); }
+			_providers[providerName] = singletonInstance;
+		}
+		/// <summary>
+		/// <para>Removes a registered factory provider.</para>
+		/// </summary>
+		/// <param name="providerName"></param>
+		public static void UnregisterFactory(string providerName)
+		{
+			if(_providers.ContainsKey(providerName))
+			{
+				_providers.Remove(providerName);
+			}
 		}
 		/// <summary>
 		/// <para>Gets a registered factory for a given provider name.</para>
@@ -29,8 +57,7 @@ namespace ADORE.Standard
 		{
 			if(!_providers.ContainsKey(providerName)) { throw new InvalidOperationException(string.Format("Factory for provider {0} is not registered.", providerName)); }
 			if(_providers[providerName] == null) { throw new InvalidOperationException(string.Format("Factory for provider {0} is not valid.", providerName)); }
-			if(!_providers[providerName].IsSubclassOf(typeof(DbProviderFactories))) { throw new InvalidOperationException(string.Format("Factory registered for provider {0} is not a System.Data.Common.DbProviderFactory.", providerName)); }
-			return (DbProviderFactory)Activator.CreateInstance(_providers[providerName]);
+			return _providers[providerName];
 		}
 	}
 }
