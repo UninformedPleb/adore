06d18be656c360fab4be7368f1db8262e61f806c
Matt Hanvelt <mhanvelt@mac.com>
2025-03-07 11:37:08 -0600

    Added Nullable<T> and DBNull handlng to DataRowExtension mapping methods, refactored slightly

--- ADORE/Extensions/DataRowExtension.cs
+++ ADORE/Extensions/DataRowExtension.cs
@@ -13,25 +13,39 @@ namespace ADORE.Extensions
 				from f in t.GetFields()
 				where !f.IsInitOnly && !f.IsStatic && !f.IsLiteral
 					&& dr.Table.Columns.Contains(f.Name)
-					&& (
-						dr.Table.Columns[f.Name].DataType.Equals(f.FieldType)
-						|| dr.Table.Columns[f.Name].DataType.IsSubclassOf(f.FieldType)
-					)
+					&& CheckTypeFit(dr.Table.Columns[f.Name].DataType, f.FieldType)
 				select f;
-			foreach(var field in fields) { field.SetValue(thing, dr[field.Name]); }
+			foreach(var field in fields)
+			{
+				if(dr[field.Name] is DBNull) { dr[field.Name] = null; }
+				field.SetValue(thing, dr[field.Name]);
+			}
 
 			var props =
 				from p in t.GetProperties()
 				where p.CanWrite
 					&& dr.Table.Columns.Contains(p.Name)
-					&& (
-						dr.Table.Columns[p.Name].DataType.Equals(p.PropertyType)
-						|| dr.Table.Columns[p.Name].DataType.IsSubclassOf(p.PropertyType)
-					)
+					&& CheckTypeFit(dr.Table.Columns[p.Name].DataType, p.PropertyType)
 				select p;
-			foreach(var prop in props) { prop.SetValue(thing, dr[prop.Name]); }
+			foreach(var prop in props)
+			{
+				if(dr[prop.Name] is DBNull) { dr[prop.Name] = null; }
+				prop.SetValue(thing, dr[prop.Name]);
+			}
 
 			return thing;
+		}
+
+		private static bool CheckTypeFit(Type src, Type dest)
+		{
+			if(src.Equals(dest)) { return true; }
+			if(src.IsSubclassOf(dest)) { return true; }
+			if(dest.IsGenericType && dest.Equals(typeof(Nullable)))
+			{
+				if(src.Equals(dest.GenericTypeArguments[0])) { return true; }
+				if(src.IsSubclassOf(dest.GenericTypeArguments[0])) { return true; }
+			}
+			return false;
 		}
 	}
 }
